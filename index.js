f10b96b21f43ee17493308af0b4514ec45a11217
Wesley Jacobs <wesleyjacobs@Wesleys-MacBook-Pro.local>
2019-09-19 16:09:42 -0700

    still not working

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
+// https://github.com/wjacobs71086/shopping-list.git
 'use strict';
 
 function getValue(){
   $('#js-shopping-list-form').submit(function( event ) {
     if (typeof $('#shopping-list-entry').val() === 'string' ){
-      $('.shopping-item').closest('li').append($('#shopping-list-entry').val());
-
-      console.log($('#shopping-list-entry').val());
+      // The Value entered in the form.
+      let enteredValue = $('#shopping-list-entry').val();
+      //console.log(enteredValue);
       event.preventDefault();
+      return enteredValue;
+      
     }
   });
 }
+// Adding the gathered value to the bottom of the list and stlye.  $('.shopping-item').last('li .shopping-item');
+function addedItem(enteredValue) {
+  //$('.shopping-item').last('li .shopping-item').append(enteredValue);
 
+}
+
+// Delete shopping Item
+function deleteItem(){
+  $('.shopping-item').click(function(){
+    this.toggleClass('.shopping-item','.shopping-item__checked');
+  });
+  console.log('im here');
+}
+
+// Check shopping Item
 
 
 
  
-
-
+$(deleteItem());
+$(addedItem());
 $(getValue());
+$(addedItem());
+
 
