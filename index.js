02a71fe24b0cf96ecedac8881e9ede4c4ef838b7
Wesley Jacobs <wesleyjacobs@Wesleys-MacBook-Pro.local>
2019-09-19 16:58:17 -0700

    getting closer

--- index.js
+++ index.js
@@ -1,40 +1,43 @@
 // https://github.com/wjacobs71086/shopping-list.git
 'use strict';
 
-function getValue(){
+
+// Adding the gathered value to the bottom of the list and stlye.  $('.shopping-item').last('li .shopping-item');
+function addedItem() {
   $('#js-shopping-list-form').submit(function( event ) {
     if (typeof $('#shopping-list-entry').val() === 'string' ){
       // The Value entered in the form.
       let enteredValue = $('#shopping-list-entry').val();
-      //console.log(enteredValue);
+      $('.shopping-item').last('li .shopping-item').append(enteredValue).addClass('.shopping-list');
+      console.log(enteredValue);
+      
       event.preventDefault();
       return enteredValue;
-      
     }
   });
+  console.log('addedItem is running');
 }
-// Adding the gathered value to the bottom of the list and stlye.  $('.shopping-item').last('li .shopping-item');
-function addedItem(enteredValue) {
-  //$('.shopping-item').last('li .shopping-item').append(enteredValue);
 
-}
 
 // Delete shopping Item
 function deleteItem(){
   $('.shopping-item').click(function(){
     this.toggleClass('.shopping-item','.shopping-item__checked');
+    event.preventDefault();
   });
-  console.log('im here');
+  console.log('Delete Item running');
 }
 
 // Check shopping Item
-
+function checkItem(){
+  console.log('Check Item Running');
+}
 
 
  
 $(deleteItem());
 $(addedItem());
-$(getValue());
-$(addedItem());
+$(checkItem());
+
 
 
