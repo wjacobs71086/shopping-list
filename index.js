df2355ac2ccb8c9f21f738f378214b86f9c22268
Wesley Jacobs <wesleyjacobs@Wesleys-MacBook-Pro.local>
2019-09-19 14:08:26 -0700

    added switch and working on value

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+switch($('js-shopping-list-form').val()){
+case 'apples':
+  console.log('apples');
+  // codeblock
+  break;
+case 'oranges':
+  // codeblock
+  break;
+case 'milk':
+  // codeblock
+  break;
+case 'bread':
+  // codeblock
+  break;
+
+default: 
+console.log($('shopping-list-entry').val());
+  // codeblock
+}
