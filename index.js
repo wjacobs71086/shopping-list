db0d3065c1db42ae95de261b27cc065a574504ab
Wesley Jacobs <wesleyjacobs@Wesleys-MacBook-Pro.local>
2019-09-19 17:52:48 -0700

    I DID IT!

--- index.js
+++ index.js
@@ -5,39 +5,65 @@
 // Adding the gathered value to the bottom of the list and stlye.  $('.shopping-item').last('li .shopping-item');
 function addedItem() {
   $('#js-shopping-list-form').submit(function( event ) {
+    event.preventDefault();
+    //console.log('addedItem is running');
     if (typeof $('#shopping-list-entry').val() === 'string' ){
       // The Value entered in the form.
       let enteredValue = $('#shopping-list-entry').val();
-      $('.shopping-item').last('li .shopping-item').append(enteredValue).addClass('.shopping-list');
-      console.log(enteredValue);
+      //$('.shopping-item').last('li .shopping-item').append(enteredValue).addClass('.shopping-list');
+      $('.shopping-list').append(generateListItem(enteredValue));
+      //console.log(enteredValue);
       
-      event.preventDefault();
       return enteredValue;
     }
   });
-  console.log('addedItem is running');
+  
+}
+function generateListItem(enteredValue){
+  return `<li>
+  <span class="shopping-item">${enteredValue}</span>
+  <div class="shopping-item-controls">
+    <button class="shopping-item-toggle">
+      <span class="button-label">check</span>
+    </button>
+    <button class="shopping-item-delete">
+      <span class="button-label">delete</span>
+    </button>
+  </div>
+</li>`;
+
 }
 
 
 // Delete shopping Item
 function deleteItem(){
-  $('.shopping-item').click(function(){
-    this.toggleClass('.shopping-item','.shopping-item__checked');
+  $('.shopping-list').on('click', '.shopping-item-delete', function(event){
     event.preventDefault();
+    $(this).closest('li').remove();
   });
-  console.log('Delete Item running');
+  
 }
 
 // Check shopping Item
 function checkItem(){
-  console.log('Check Item Running');
+  //console.log('Check Item Running');
+  $('.shopping-list').on('click', '.shopping-item-toggle', function(event){
+    event.preventDefault();
+    let spanItem = $(this).closest('li').children('span');
+    $(spanItem).toggleClass('shopping-item__checked');
+  });
 }
 
 
- 
-$(deleteItem());
-$(addedItem());
-$(checkItem());
+function init(){
+  deleteItem();
+  addedItem();
+  checkItem();
+}
+
+$(init)
+
+
 
 
 
